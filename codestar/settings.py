5bc6272dd4ba8da70afe95d6a77129f642ecd213
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-02-05 09:41:54 +0000

    add POST logic for submitting a comment and a success message for UX

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-0f$66dvsb*@drrx(i-iz+kruujwj=5&hr#)cvr+mj45_aw!u_)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", '.herokuapp.com']
 
