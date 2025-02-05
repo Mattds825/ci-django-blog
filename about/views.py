7094d81ca9c737b27def83bdf27afd56cb592318
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-02-05 10:22:42 +0000

    add code to handle collabation request form POST method

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -8,9 +9,18 @@ def about_me(request):
     """
     Renders the About page
     """
+    collaborate_form = CollaborateForm()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request submitted successfully, please await a response"
+            )
+    
     about = About.objects.all().order_by("-updated_on").first()
     collaborate_form = CollaborateForm()
-    
     return render(
         request,
         "about/about.html",
